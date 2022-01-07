275faf3fb8f01ac9439283a619b1fd0ae9b24e38
Shola Nejo <sholanejo@live.com>
2022-01-07 14:16:59 +0100

    feat: added filter by category

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Linq;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -14,11 +14,12 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public IActionResult Index(int productPage = 1)
+        public ViewResult Index(string category, int productPage = 1)
         {
             return View(new ProductsListViewModel
             {
                 Products = repository.Products
+                .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((productPage - 1) * PageSize)
                     .Take(PageSize),
